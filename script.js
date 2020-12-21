f19fd115cdaf7e60d56db54202cea1e58630518c
Mykyta Bashenko <45737720+MykytaBAshenko@users.noreply.github.com>
2020-12-21 17:01:08 +0200

    dasd

--- script.js
+++ script.js
@@ -87,7 +87,7 @@ let menu_obj = [
         ]
       },
       {
-        podcategory_name:"podcategory1",
+        podcategory_name:"podcategory2",
         links_of_podcategory: [
           {
             name_of_podcategory: "link1",
@@ -108,7 +108,7 @@ let menu_obj = [
         ]
       },
       {
-        podcategory_name:"podcategory1",
+        podcategory_name:"podcategory34",
         links_of_podcategory: [
           {
             name_of_podcategory: "link1",
@@ -132,7 +132,7 @@ let menu_obj = [
     
   },
   {
-    name_of_category: "category1",
+    name_of_category: "category2",
     svg_icon: `    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"	 viewBox="0 0 477.534 477.534" style="enable-background:new 0 0 477.534 477.534;" xml:space="preserve"><g>	<g>		<path d="M438.482,58.61c-24.7-26.549-59.311-41.655-95.573-41.711c-36.291,0.042-70.938,15.14-95.676,41.694l-8.431,8.909			l-8.431-8.909C181.284,5.762,98.662,2.728,45.832,51.815c-2.341,2.176-4.602,4.436-6.778,6.778			c-52.072,56.166-52.072,142.968,0,199.134l187.358,197.581c6.482,6.843,17.284,7.136,24.127,0.654			c0.224-0.212,0.442-0.43,0.654-0.654l187.29-197.581C490.551,201.567,490.551,114.77,438.482,58.61z M413.787,234.226h-0.017			L238.802,418.768L63.818,234.226c-39.78-42.916-39.78-109.233,0-152.149c36.125-39.154,97.152-41.609,136.306-5.484			c1.901,1.754,3.73,3.583,5.484,5.484l20.804,21.948c6.856,6.812,17.925,6.812,24.781,0l20.804-21.931			c36.125-39.154,97.152-41.609,136.306-5.484c1.901,1.754,3.73,3.583,5.484,5.484C453.913,125.078,454.207,191.516,413.787,234.226			z"/>	</g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg>`,
     podcategoryes: [
       {
@@ -239,13 +239,14 @@ set_category(0)
 
 function set_category(n) {
   let podcategory_list = document.getElementById("category_list")
+  podcategory_list.innerHTML = ""
   let result = []
   for(let pp = 0; pp < menu_obj[n].podcategoryes.length;pp++ ){
     let podcategory_element = document.createElement("div")
     podcategory_element.className = "podcategory_element"
     let category_element_title = document.createElement("div")
     category_element_title.className = "category_element_title"
-    category_element_title.innerHTML = menu_obj[n].podcategory_name
+    category_element_title.innerHTML = menu_obj[n].podcategoryes[pp].podcategory_name
     podcategory_element.append(category_element_title)
     let podcategory_list_exact = document.createElement("div")
     podcategory_list_exact.className = "podcategory_list_exact"
@@ -254,7 +255,7 @@ function set_category(n) {
       podcategory_list_elem.href = menu_obj[n].podcategoryes[pp].links_of_podcategory[hhh].link_of_podcategory;
       podcategory_list_elem.innerHTML = menu_obj[n].podcategoryes[pp].links_of_podcategory[hhh].name_of_podcategory;
       podcategory_list_elem.className  = "podcategory_list_elem_link"
-      podcategory_list_exact.append("podcategory_list_elem")
+      podcategory_list_exact.append(podcategory_list_elem)
     }
     podcategory_element.append(podcategory_list_exact)
     podcategory_list.append(podcategory_element)
